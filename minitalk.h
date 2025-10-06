8345c331ac21a19054088dc59f3e00e0ac9deece
dylina <109595463+dylina@users.noreply.github.com>
2025-10-06 10:44:47 +0200

    Add files via upload

--- minitalk.h
+++ minitalk.h
@@ -6,15 +6,15 @@
 /*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 13:45:24 by dgorceac          #+#    #+#             */
-/*   Updated: 2025/09/16 15:54:27 by dgorceac         ###   ########.fr       */
+/*   Updated: 2025/10/06 10:26:00 by dgorceac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include "../printf/ft_printf.h"
-# include "../printf/libft/libft.h"
+# include "printf/ft_printf.h"
+# include "printf/libft/libft.h"
 # include <unistd.h>
 # include <stdio.h>
 # include <signal.h>
